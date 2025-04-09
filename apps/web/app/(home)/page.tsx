90c1d730f4f31561d66779f08559277c31412fb3
putraridho <muhammadridhoputra@gmail.com>
2025-04-10 01:05:49 +0700

    feat: add user authentication check to HomePage component

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,9 +1,15 @@
+import { TOKEN_KEY } from "@convertium/constants";
 import { Container, Header, VStack } from "@convertium/ui";
+import { cookies } from "next/headers";
 
 export default function HomePage() {
+  const cookieStore = cookies();
+
+  const token = cookieStore.get(TOKEN_KEY)?.value;
+
   return (
     <VStack className="min-h-screen" gap={0}>
-      <Header />
+      <Header loggedIn={!!token} />
       <Container className="pt-8">
         <h1>Welcome to the Home Page</h1>
         <p>This is the main page of your Next.js application.</p>
